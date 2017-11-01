c4dce5b00405c0ab5166f98126cbae7a036d4693
Maxim Shpilka <pisik8809@gmail.com>
2017-11-01 17:28:46 +0200

    1-st commit

---
+++ tic-tac-toe.js
@@ -0,0 +1,14 @@
+var count = setTimeout(function autoHod() {
+  var comp = [];
+  comp = document.getElementsByClassName('field');
+  console.log(comp);
+  (function getRandom() {
+  var min = 0;
+  var max = comp.length - 1;
+  var rand = min + Math.floor(Math.random() * (max + 1 - min));
+  console.log(rand);
+  comp[rand].innerHTML += '<p>0</p>';
+  comp[rand].setAttribute('class', 'comp');
+}());
+count = setTimeout(autoHod, 8000);
+}, 2000);
